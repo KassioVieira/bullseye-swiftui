66cfbd4cc2be9c6d2c532831d8b956d2aaa68885
KassioVieira <kassio.vieira7@gmail.com>
2021-01-05 18:31:17 -0300

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Kássio Luz on 05/01/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
